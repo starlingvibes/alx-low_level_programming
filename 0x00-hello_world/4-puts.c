e91c67a859d5ba84a56ff5fc26093862519e41e0
starlingvibes <starlingroot@protonmail.com>
2022-04-07 11:47:22 -0700

    added description for main function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 /**
- * main function - prints the required phrase
+ *   main - prints the phrase "Programming is like building a multilingual puzzle
+ *   Return: 0 if exited properly, non-zero otherwise
+ *   
  */
 int main(void)
 {
