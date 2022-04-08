6e6580054753c71f25149a5c1d3b86386e0480d2
starlingvibes <starlingroot@protonmail.com>
2022-04-08 08:23:26 -0700

    added a newline character

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,15 +16,15 @@ int main(void)
 	z = n % 10;
 	if (z > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5", n, z);
+		printf("Last digit of %d is %d and is greater than 5\n", n, z);
 	}
 	else if (z == 0)
 	{
-		printf("Last digit of %d is %d and is 0", n, z);
+		printf("Last digit of %d is %d and is 0\n", n, z);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0", n, z);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, z);
 	}
 	return (0);
 }
