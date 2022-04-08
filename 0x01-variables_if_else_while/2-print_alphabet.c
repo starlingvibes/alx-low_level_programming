936c970b45f07aec6237a256b148e241a44e6b51
starlingvibes <starlingroot@protonmail.com>
2022-04-08 08:53:51 -0700

    lowercase letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - prints all lowercase letters
+ * Return: returns 0 (success)
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++);
+	{
+		putchar(ch);
+	}
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,3 @@
 conditional statements in c
+getting the last digit of a number
+printing all lowercase letters
