c7ebf1eb0418722bf5690fa790647fe23d422009
starlingvibes <starlingroot@protonmail.com>
2022-04-08 10:40:43 -0700

    printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - returns numbers from 0 to 9
+ * Return: returns 0 (success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf('%d', x)
+	}
+	putchar('\n');
+	return (0);
+}
