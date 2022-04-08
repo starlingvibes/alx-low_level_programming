363b9cb8968cda85d1fe235aa708e87e0ec5b6b5
starlingvibes <starlingroot@protonmail.com>
2022-04-08 09:54:41 -0700

    small and capital alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - returns small then capital alphabets
+ * Return: returns 0 (success)
+ */
+int main(void)
+{
+	char ch;
+	char up;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+		for (up = 'A'; up <= 'Z'; up++)
+		{
+			putchar(up);
+		}
+	}
+	putchar("\n")
+	return (0);
