8e917c4d405129481fab36d744d5692d33a2db25
starlingvibes <starlingroot@protonmail.com>
2022-04-08 11:00:25 -0700

    lowercase letters in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ *	main - prints all lowercase letters in reverse
+ *	Return: returns 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
