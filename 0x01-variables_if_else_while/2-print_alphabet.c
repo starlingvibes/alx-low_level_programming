a09194f9d527c7f3f0923520664b5f2e0ab2a9bd
starlingvibes <starlingroot@protonmail.com>
2022-04-08 09:42:05 -0700

    lowercase letters

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-		char ch;
-		for (ch = 'a'; ch <= 'z'; ch++)
-		{
-			putchar(ch);
-		}
-		putchar('\n');
-		return (0);
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
