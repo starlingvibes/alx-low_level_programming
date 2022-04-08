61a5035d8c5bdeb42a1be5ced94e0b77816f3a52
starlingvibes <starlingroot@protonmail.com>
2022-04-08 10:23:25 -0700

    lowercase letters excluding q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - returns lowercase letters excluding q and e
+ * Return: returns 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'e' || 'q')
+		{
+			ch = '\0';
+		} else {
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
