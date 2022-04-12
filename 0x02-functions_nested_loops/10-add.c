003b291aa837910da6cad0802c6d95c51e027eb4
starlingvibes <starlingroot@protonmail.com>
2022-04-12 10:41:48 -0700

    adds two integers and outputs the result

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,6 +2,9 @@
 
 /**
  * add - sums two integers and outputs the result
+ * @x: the first integer to be added
+ * @y: the second integer to be added
+ * Return: returns the sum of the two integers
  */
 int add(int x, int y)
 {
