76e36260b248d1b217ed165c70e7610ffc3ee93e
starlingvibes <starlingroot@protonmail.com>
2022-04-12 08:12:53 -0700

    10x uppercase letters

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,16 @@ void print_alphabet_x10(void)
 {
 	char le;
 	int x;
-	x = 1;
+	x = 0;
 
-	while (x <= 10)
+	while (x < 10)
 	{
 		le = 'a';
 
 		while (le <= 'z')
 		{
 			_putchar(le);
+			le++;
 		}
 		_putchar('\n');
 		x++;
