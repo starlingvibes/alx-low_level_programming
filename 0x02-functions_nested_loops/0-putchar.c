e5f5399a2c93b8c54288fafa6d9c08787c1b6bc8
starlingvibes <starlingroot@protonmail.com>
2022-04-12 07:39:14 -0700

    displaying putchar)

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,15 @@
  */
 int main(void)
 {
-	fwrite("_putchar");
-	fwrite("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,1 @@
-#include <stdio.h>
+int _putchar(char c);
