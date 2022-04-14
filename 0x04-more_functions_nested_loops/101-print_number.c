181f95b9acc15a20556b24a519454e0dd94f7d64
starlingvibes <starlingroot@protonmail.com>
2022-04-14 12:01:57 -0700

    print an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,6 +7,21 @@
 
 void print_number(int n)
 {
-	_putchar(n + '0');
-	_putchar('\n');
+	unsigned int n1;
+
+	if (n < 0)
+	{
+		n1 = -n;
+		_putchar('-');
+	} else
+	{
+		n1 = n;
+	}
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
 }
