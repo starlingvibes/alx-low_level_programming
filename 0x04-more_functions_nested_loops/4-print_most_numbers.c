c942c546035980d5b0a1a1b95e0445ab1d131189
starlingvibes <starlingroot@protonmail.com>
2022-04-14 07:31:00 -0700

    do not print 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers except 2 and 4
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
