aad3db89bdc13d792dc222d7035c7d8a2a543924
starlingvibes <starlingroot@protonmail.com>
2022-04-14 08:55:51 -0700

    the fizzbuzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,17 +13,23 @@ int main(void)
 	{
 		if (i % 15 == 0)
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz ");
 		} else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz ");
 		} else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
+		} else if (i == 1)
+		{
+			printf("%d", i);
 		} else
 		{
-			printf("%d ", i);
+			printf(" %d", i);
 		}
 	}
+
 	printf("\n");
+
+	return (0);
 }
