f5b7ea1d22257aadfe486ce6cc2bc6be07084ee2
starlingvibes <starlingroot@protonmail.com>
2022-04-14 07:03:44 -0700

    return the product of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - returns the product of two integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 function that checks for uppercase character
 function that checks for a digit
+function that multiplies two integers
