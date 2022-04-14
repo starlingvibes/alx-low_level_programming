4188fae2707e15a91daa3e536f8f760dcc937e07
starlingvibes <starlingroot@protonmail.com>
2022-04-14 01:09:29 -0700

    fix leap year issue

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,15 +11,15 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 100 == 0 || year % 400 == 0) && (year % 4 == 0))
+	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
 	{
-		if (month > 2 && day >= 60)
+		if (month >= 2 && day >= 60)
 		{
 			day++;
 		}
 
 		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 365 - day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
