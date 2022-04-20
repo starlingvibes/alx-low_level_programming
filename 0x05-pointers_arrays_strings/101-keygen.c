533ba5b5e7c30e93087487dff5773e51024f62c1
starlingvibes <starlingroot@protonmail.com>
2022-04-20 11:32:40 -0700

    password generator

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - function that generates random valid passwords
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int pass[100];
+	int i, sum, n;
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand % 87;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 87)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+
+	return (0);
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -10,3 +10,4 @@ function that prints n elements of an array of integers
 prints n elements of an array of integers
 function that copies the string pointed to by src, including the null byte to the buffer pointed to by dest
 function that converts a string to an integer
+program that generates random valid passwords
