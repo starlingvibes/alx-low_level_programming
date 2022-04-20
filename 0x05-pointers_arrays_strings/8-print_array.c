5f22ff5b29c14d87ee2d2f5e7c09fae508895636
starlingvibes <starlingroot@protonmail.com>
2022-04-20 10:39:48 -0700

    prints e elements of an array of integers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,9 +9,14 @@
 void print_array(int *a, int n)
 {
 	int i;
+	int index = -1;
 
 	for (i = 0; i < n; i++)
 	{
+		if (a[i] == a[index])
+		{
+			printf("%d", a[i]);
+		}
 		printf("%d, ", a[i]);
 	}
 	putchar('\n');
