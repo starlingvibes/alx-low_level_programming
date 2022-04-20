fb40f1c8a3487f1b2c62b4890f4117375c4cd66c
starlingvibes <starlingroot@protonmail.com>
2022-04-20 10:58:39 -0700

    copies a string including null byte to the buffer

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,5 +8,19 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	strcpy(dest, src);
+	int len, i;
+
+	len = 0;
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
 }
