ff8c3c9b84884d2218cb3f62952b29f256721115
starlingvibes <starlingroot@protonmail.com>
2022-04-26 07:35:27 -0700

    gets the length of a prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: given substring
+ * @accept: string to check against
+ * Return: number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, f, flag;
+
+	f = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		flag = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				f++;
+				flag = 1;
+			}
+		}
+		if (flag == 0)
+		{
+			return (f);
+		}
+	}
+
+	return (0);
+}
