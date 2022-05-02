19c732ac80337af0d5f63a098e58da2ed3108d21
starlingvibes <starlingroot@protonmail.com>
2022-05-02 16:32:04 -0700

    multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - multiplies two numbers
+ * @argc: argument count
+ * @argv: the argument vector
+ * Return: 1 on error, 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		int res;
+
+		res = argv[1] * argv[2];
+		printf("%d\n", res);
+	}
+	
+	return (0);
+}
