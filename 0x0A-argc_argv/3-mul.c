35c3e7bf947b3a0b4ed7a3265b850fb58faa5b50
starlingvibes <starlingroot@protonmail.com>
2022-05-02 16:44:31 -0700

    multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -18,8 +18,8 @@ int main(int argc, char *argv[])
 	{
 		int num1, num2, res;
 
-		num1 = *argv[1];
-		num2 = *argv[2];
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
 		res = num1 * num2;
 
 		printf("%d\n", res);
