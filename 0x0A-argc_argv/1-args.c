6a4a59e4b7abebc9a1f8e191cfad40555940950a
starlingvibes <starlingroot@protonmail.com>
2022-05-02 16:13:13 -0700

    print number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed to it
+ * @argc - the argument count
+ * @argv - the argument vector
+ * Return: 0
+ */
+
+int main(char argc, __attribute__((unused)), char argv[])
+{
+	printf("%s\n", *argc);
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,1 +1,2 @@
 program that prints its name, followed by a new line
+program that prints the number of arguments passed into it
