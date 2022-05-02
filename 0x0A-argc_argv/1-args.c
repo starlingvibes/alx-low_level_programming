49152e8e1e290153dddfd907b4580ffc505ba4ed
starlingvibes <starlingroot@protonmail.com>
2022-05-02 16:15:17 -0700

    print number of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,8 +7,8 @@
  * Return: 0
  */
 
-int main(char argc, __attribute__((unused)), char argv[])
+int main(int argc, __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", *argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
