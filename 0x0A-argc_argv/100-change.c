21573a1210687902fea1fed84c4e40de3a25df08
starlingvibes <starlingroot@protonmail.com>
2022-05-02 17:17:48 -0700

    make change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * main - prints minimum number of coins to make change for an amount
+ * @argc: number of arguments
+ * @argv: argument vector
+ *
+ * Return: 0 if no errors, else 1
+ */
+int main(int argc, char *argc[])
+{
+	int a, n = 0, i, t;
+
+	int c[5] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		puts("Error");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	if (a <= 0)
+	{
+		puts("0");
+		return (1);
+	}
+	else
+	{
+		for (i = 0; i < 5; i++)
+		{
+			t = a / c[i];
+			a -= t * c[i];
+			n += t;
+			if (a == 0)
+				break;
+		}
+	}
+	printf("%d\n", n);
+	return (0);
+}
