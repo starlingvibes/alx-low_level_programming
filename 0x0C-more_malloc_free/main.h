c5ceafa4dce6e471ac81086897a130b9329f23d9
starlingvibes <starlingroot@protonmail.com>
2022-05-09 14:20:40 -0700

    updated prototypes

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,3 +7,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int is_digit(char *s);
+int _strlen(char *s);
+void errors(void);
+int main(int argc, char *argv[]);
