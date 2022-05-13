9a9a0a3cc3222ce1840dc94cbf1d4557224ca09d
starlingvibes <starlingroot@protonmail.com>
2022-05-13 10:07:46 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to a function
+ *
+ * Return: NIL
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	printf("%s\n", name);
+}
