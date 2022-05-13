3f93dc24d7c00a92e7e21c4b8571de4daaabea79
starlingvibes <starlingroot@protonmail.com>
2022-05-13 10:12:41 -0700

    added standard libraries

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,11 +5,11 @@
  * @name: name to print
  * @f: pointer to a function
  *
- * Return: NIL
- *
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	printf("%s\n", name);
+	if (!name || !f)
+		return;
+	f(name);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,1 +1,4 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 void print_name(char *name, void (*f)(char *));
