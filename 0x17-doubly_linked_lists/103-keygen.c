47e0eb01a243c91ed9d2b3874dc96702efe26948
starlingvibes <starlingroot@protonmail.com>
2022-07-07 14:44:36 -0700

    removed \n

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,44 +1,123 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include "lists.h"
 
 /**
- * main - generate a key depending on a username for crackme5
- * @argc: number of arguments passed
- * @argv: arguments passed to main
- * Return: 0 on success, 1 on error
+ * _strlen - returns the length of the string
+ * @s: takes string
+ *
+ * Return: returns the length int
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+	while (s[count])
+	{
+		count++;
+	}
+	return (count);
+}
+/**
+ * f4 - gen
+ * @len: length
+ * @argv:arguments
+ * Return: int
+ */
+int f4(int len, char *argv)
+{
+	int i = 0, j = 0;
+
+	while (i < len)
+	{
+		if (argv[i] > j)
+			j = argv[i];
+		i++;
+	}
+	srand(j ^ 14);
+	i = rand();
+	return (i);
+}
+/**
+ * f5 - gen
+ * @len:length
+ * @argv:arguments
+ * Return: int
+ */
+
+int f5(int len, char *argv)
+{
+	int i = 0;
+	long int j = 0;
+
+	while (i < len)
+	{
+		j += argv[i] * argv[i];
+		j = (int)j;
+		i++;
+	}
+	j = (j ^ 239) & 63;
+	return (j);
+}
+/**
+ * f6 - gen
+ * @argv:arguments
+ * Return: int
+ */
+
+int f6(char *argv)
+{
+	int i = 0;
+	long int j = 0;
+
+	while (i < argv[0])
+	{
+		j = rand();
+		i++;
+	}
+	j = (j ^ 229) & 63;
+	return (j);
+
+}
+/**
+ * main - Generates and prints passwords for the crackme5 executable.
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-	unsigned int i, b;
-	size_t len, add;
-	char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
-	char p[7] = "      ";
+	int i = 0;
+	long int st = 0;
+	int len = 0;
+	char arr[7] = {0};
+	char t[] = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
 	if (argc != 2)
+		return (-1);
+	len = _strlen(argv[1]);
+	arr[0] = t[(len ^ 59) & 63];
+
+	while (i < len)
 	{
-		printf("Correct usage: ./keygen5 username\n");
-		return (1);
+		st += argv[1][i];
+		i++;
 	}
-	len = strlen(argv[1]);
-	p[0] = l[(len ^ 59) & 63];
-	for (i = 0, add = 0; i < len; i++)
-		add += argv[1][i];
-	p[1] = l[(add ^ 79) & 63];
-	for (i = 0, b = 1; i < len; i++)
-		b *= argv[1][i];
-	p[2] = l[(b ^ 85) & 63];
-	for (b = argv[1][0], i = 0; i < len; i++)
-		if ((char)b <= argv[1][i])
-			b = argv[1][i];
-	srand(b ^ 14);
-	p[3] = l[rand() & 63];
-	for (b = 0, i = 0; i < len; i++)
-		b += argv[1][i] * argv[1][i];
-	p[4] = l[(b ^ 239) & 63];
-	for (b = 0, i = 0; (char)i < argv[1][0]; i++)
-		b = rand();
-	p[5] = l[(b ^ 229) & 63];
-	printf("%s\n", p);
+	arr[1] = t[(st ^ 79) & 63];
+	st = 1, i = 0;
+	while (i < len)
+	{
+		st *= argv[1][i];
+		st = (int)st;
+		i++;
+	}
+	arr[2] = t[(st ^ 85) & 63];
+	arr[3] = t[f4(len, argv[1]) & 63];
+	arr[4] = t[f5(len, argv[1])];
+	arr[5] = t[f6(argv[1])];
+	printf("%s", arr);
 	return (0);
 }
