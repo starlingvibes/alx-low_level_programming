fdf95c48226c1f9fddaa592aaf001bc38b2a68eb
starlingvibes <starlingroot@protonmail.com>
2022-07-07 11:06:57 -0700

    print list count

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,6 +10,11 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nodes = 0;
 
+	if (!h)
+	{
+		return (0);
+	}
+
 	while (h)
 	{
 		printf("%d\n", h->n);
--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -20,4 +20,4 @@ size_t dlistint_len(const dlistint_t *h)
 	}
 
 	return (nodes);
-}
+i}
