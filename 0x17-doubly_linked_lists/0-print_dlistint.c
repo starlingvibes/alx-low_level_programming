7c477193a9909bcfcd43b18fe8aec8ad5dfac86d
starlingvibes <starlingroot@protonmail.com>
2022-07-07 09:05:52 -0700

    print and count elements of a doubly linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints and counts a doubly linked list
+ * @h: pointer to the head node
+ *
+ * Return: number of nodes in list
+ */ 
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes = 0
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodes++;
+	}
+
+	return (nodes);
+}
