2f4653485b3d2f2ba56a0c387101ef8e04c45f3e
Chidera Anichebe <starlingroot@protonmail.com>
2022-07-07 22:12:15 +0100

    keygen for crackme5

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -6,6 +6,7 @@
  * main - Generates and prints passwords for the crackme5 executable.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
+ *
  * Return: Always 0.
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
