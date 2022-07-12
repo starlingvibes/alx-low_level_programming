d76340a198d25fcb3330fb01b916b0158a51a6d0
starlingvibes <starlingroot@protonmail.com>
2022-07-12 08:20:05 -0700

    updated url

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/starlingvibes/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libmask.so
+wget -P /tmp https://github.com/starlingvibes/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libmask.so
 export LD_PRELOAD=/tmp/libmask.so
