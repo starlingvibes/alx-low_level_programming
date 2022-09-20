4b392f501cc2c44cd52d648aa54c2877c9d40b21
starlingvibes <starlingroot@protonmail.com>
2022-09-20 05:45:52 -0700

    perimeter of island in a grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python3
 """ Module that calculates the perimeter of an island in a grid """
 
-def neighbours(grid, i, j):
-    """ Returns the number of water neighbours """
-
-    num = 0
-    if i <= 0 or not grid[i - 1][j]:
-        num += 1
-    if j <= 0 or not grid[i][j - 1]:
-        num += 1
-    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
-        num += 1
-    if i >= len(grid) - 1 or not grid[i + 1][j]:
-        num += 1
-
-    return num
-
 def island_perimeter(grid):
     """ Returns the perimeter of the island in grid """
+    visit = set()
+    def dfs(i, j):
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 0:
+            return 1
+        if (i, j) in visit:
+            return 0
+        
+        visit.add((i, j))
+        perimeter = dfs(i, j + 1)
+        perimeter += dfs(i + 1, j)
+        perimeter += dfs(i, j - 1)
+        perimeter += dfs(i - 1, j)
+        return perimeter
 
-    perimeter = 0
-    for i in range(grid):
-        for j in range(grid[0]):
-            if grid[i][j]:
-                perimeter += neighbours(grid, i, j)
-
-    return perimeter
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                return dfs(i, j)
